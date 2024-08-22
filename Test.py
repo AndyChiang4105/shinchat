cbebbd4726e14a84e1438dd309af283d4e2d7612
shinnosuke <scushinnosuke@gmail.com>
2024-08-22 13:20:15 +0800

    some modify

--- Test.py
+++ Test.py
@@ -1,6 +1,17 @@
-from soVITS_api import getVitsResponse
+from RAG_ChatGPT import stream_sentences
+for sentence in stream_sentences("關公刮骨療毒是真的嗎?"):
+        if sentence:
+            print(sentence)
 
-text = '曹操的勢力不及元紹'
-output_bytesIO = getVitsResponse(text)
-with open('output.wav', "wb") as f:
-    f.write(output_bytesIO.read())
+# from langchain_community.vectorstores import Chroma
+# from langchain_openai import OpenAIEmbeddings
+
+# base_url = "https://api.chatanywhere.cn/v1"
+# embedding_model = OpenAIEmbeddings(base_url=base_url)
+# chroma_db_path = "Three_Kingdoms_storage"
+# vectorstore = Chroma(persist_directory=chroma_db_path, embedding_function=embedding_model)
+
+# q = '赤壁之戰'
+
+# docs = vectorstore.similarity_search(q)
+# print(len(docs))
